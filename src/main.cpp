66c1cc2ec33209ab0de1653d0acb24fffea7cdc5
Diyor Sattarov <diyorsattarov@outlook.com>
2025-04-27 15:33:45 -0500

    feat: Add rotation to square

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,20 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <OpenGL/glu.h>
+#include <cmath>
+#include <ctime>
 #include <iostream>
 
 int g_iWindowWidth = 1000, g_iWindowHeight = 800;
 GLFWwindow *g_pWindow = nullptr;
+clock_t g_PreviousTicks, g_CurrentTicks;
+float g_fRotationRate = 50.0f;
+float g_fRotate1 = 0.0f;
+
+void FramebufferSizeCallback(GLFWwindow *window, int width, int height);
+void ProcessInput(GLFWwindow *window);
+void DrawUnitSquare();
+void UpdateDeltaTime();
 
 void FramebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
@@ -13,9 +23,21 @@ void FramebufferSizeCallback(GLFWwindow *window, int width, int height)
     g_iWindowWidth = width;
     g_iWindowHeight = height;
     glViewport(0, 0, width, height);
+    glLoadIdentity();
+    gluPerspective(60.0f, (double)width / height, 0.1, 100.0);
 }
 
 void ProcessInput(GLFWwindow *window) { if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true); }
+
+void UpdateDeltaTime()
+{
+    g_CurrentTicks = clock();
+    float deltaTicks = (float)(g_CurrentTicks - g_PreviousTicks);
+    g_PreviousTicks = g_CurrentTicks;
+    float fDeltaTime = deltaTicks / (float)CLOCKS_PER_SEC;
+    g_fRotate1 += (10 * g_fRotationRate * fDeltaTime);
+    g_fRotate1 = fmodf(g_fRotate1, 360.0f);
+}
 
 void DrawUnitSquare()
 {
@@ -41,7 +63,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
     g_pWindow = glfwCreateWindow(g_iWindowWidth, g_iWindowHeight,
-                                 "Square Render", nullptr, nullptr);
+                                 "Rotating Square Render", nullptr, nullptr);
 
     if (!g_pWindow)
     {
@@ -68,14 +90,21 @@ int main()
     gluPerspective(60.0f, (float)g_iWindowWidth / g_iWindowHeight, 0.1, 100.0);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+    g_PreviousTicks = clock();
+
     while (!glfwWindowShouldClose(g_pWindow))
     {
         ProcessInput(g_pWindow);
+        UpdateDeltaTime();
+
         glClear(GL_COLOR_BUFFER_BIT);
         
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
         glTranslatef(0.0f, 0.0f, -6.0f);
+    
+        glRotatef(g_fRotate1, 0.0f, 1.0f, 0.0f);
+
         glColor3f(1.0f, 0.0f, 0.0f);
         DrawUnitSquare();
 
